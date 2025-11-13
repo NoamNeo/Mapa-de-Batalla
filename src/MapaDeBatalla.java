7be49049fd0fe52055be9665681610baa13a86f8
NoamNeo <gabriel.loira.lorenzo@gmail.com>
2025-11-13 13:27:29 +0100

    El usuario ahora puede mover el jugador a través del tablero

--- src/MapaDeBatalla.java
+++ src/MapaDeBatalla.java
@@ -2,6 +2,66 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class MapaDeBatalla {
+  public static void game(int[][] mapa, GamePiece jugador, GamePiece enemigo, Scanner usrInput) {
+    int usrChoice;
+    boolean partida = true;
+    System.out.println("Estado inicial del mapa");
+    imprimirMapa(mapa);
+    while (partida) {
+      System.out.println("Escribe la acción que quieres hacer:");
+      System.out.println("1: Moverse");
+      System.out.println("2: Disparar");
+      usrChoice = usrInput.nextInt();
+      switch (usrChoice) {
+        case 1:
+          moverJugador(mapa, jugador, usrInput);
+          break;
+        case 2:
+          break;
+
+      }
+      imprimirMapa(mapa);
+    }
+    usrInput.close();
+  }
+
+  public static void setPosicion(int[][] mapa, int newPos, int oldPos) {
+    int swap;
+    swap = mapa[oldPos / mapa.length][oldPos % mapa.length];
+    mapa[oldPos / mapa.length][oldPos % mapa.length] = 0;
+    mapa[(oldPos + newPos) / mapa.length][(oldPos + newPos) % mapa.length] = swap;
+  }
+
+  public static void moverJugador(int[][] mapa, GamePiece jugador, Scanner usrInput) {
+    // Chuleta W = -N ; S = +N ; A = -1 ; D = +1 ; N = 0
+    String usrChoice;
+    System.out.println("\nEscribe que quieres hacer");
+    System.out.println("W: moverse hacia arriba");
+    System.out.println("A: moverse hacia la izquierda");
+    System.out.println("S: moverse hacia abajo");
+    System.out.println("D: moverse hacia la derecha");
+    System.out.println("N: no moverse");
+    usrInput.nextLine();
+    usrChoice = usrInput.nextLine();
+    switch (usrChoice) {
+      case "W":
+        setPosicion(mapa, -(mapa.length), jugador.pos);
+        jugador.pos = jugador.pos - mapa.length;
+        break;
+      case "S":
+        setPosicion(mapa, mapa.length, jugador.pos);
+        jugador.pos = jugador.pos + mapa.length;
+        break;
+      case "A":
+        setPosicion(mapa, -1, jugador.pos);
+        jugador.pos = jugador.pos - 1;
+        break;
+      case "D":
+        setPosicion(mapa, 1, jugador.pos);
+        jugador.pos = jugador.pos + 1;
+    }
+  }
+
   public static void imprimirMapa(int[][] mapa) {
     for (int i = 0; i < mapa.length; i++) {
       for (int j = 0; j < mapa.length; j++) {
@@ -20,24 +80,26 @@ public class MapaDeBatalla {
   public static void main(String[] args) {
     Random rand = new Random();
     Scanner usrInput = new Scanner(System.in);
+    GamePiece jugador = new GamePiece();
+    GamePiece enemigo = new GamePiece();
     int[][] mapa;
-    int usrChoice, randInt, buffer;
+    int usrChoice, randInt;
     boolean isDiff = false;
     System.out.println("Escribe el tamaño del tablero para la partida:");
     usrChoice = usrInput.nextInt();
     mapa = new int[usrChoice][usrChoice];
     randInt = rand.nextInt((usrChoice * usrChoice));
-    buffer = randInt;
-    mapa[buffer / usrChoice][buffer % usrChoice] = 1;
+    jugador.pos = randInt;
+    mapa[jugador.pos / usrChoice][jugador.pos % usrChoice] = 1;
     while (!isDiff) {
       randInt = rand.nextInt((usrChoice * usrChoice));
-      if (randInt != buffer) {
+      if (randInt != jugador.pos) {
         isDiff = true;
       }
     }
-    buffer = randInt;
-    mapa[buffer / usrChoice][buffer % usrChoice] = 2;
-    imprimirMapa(mapa);
+    enemigo.pos = randInt;
+    mapa[enemigo.pos / usrChoice][enemigo.pos % usrChoice] = 2;
+    game(mapa, jugador, enemigo, usrInput);
     usrInput.close();
   }
 }
